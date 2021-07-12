6b445c9a08a883c732b795d3550e0590f31cd998
Pfizz <pfiste80@gmail.com>
2021-07-12 02:35:57 -0400

    end of 4.2.9

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,11 @@ var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
     // packagae data up as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -12,6 +17,7 @@ var taskFormHandler = function(event) {
     };
     //send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
+    formEl.reset();
 }; 
 
 var createTaskEl = function(taskDataObj) {
